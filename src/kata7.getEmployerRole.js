0f3b7f4197abcead857b7886079653e7c5274a47
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-25 23:31:02 +0200

    wrote the test and the solution for get employerrole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,11 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    for (let i = 0; i < employees.length; i++) {
+        if(employees[i]["name"] === employeeName){
+            return employees[i]["role"];
+        }
+    }
+    
+    return "There is no such employee";
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,30 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  it("returns the employee's role in the company", () => {});
 });
+
+describe("getEmployerRole", () => {
+  const employees = [{
+
+    name: 'Satti',
+  
+    role: 'Developer'
+  
+  }, {
+  
+    name: 'Jenny',
+  
+    role: 'Sales Associate'
+  
+  }, {
+  
+    name: 'Javid',
+  
+    role: 'Human Recommended Reading Assistant'
+  
+  }]
+
+  expect(getEmployerRole("Javid", employees)).toBe("Human Recommended Reading Assistant");
+  expect(getEmployerRole("Tugcan", employees)).toBe("There is no such employee");
+}) 
