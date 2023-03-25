6cb348325c9627bd9611ab6c5fc2294fec3c1414
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-25 22:51:58 +0200

    wrote the test and solution for the reachdestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    var time = Math.round((distance/speed) / 0.5) * 0.5;
+    return 'I should be there in ' + time + " hours.";
+};
 
 module.exports = reachDestination;
