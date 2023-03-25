9ef0d3aca6b1c44271717e36aceeb595e82bc950
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-25 22:41:41 +0200

    wrote the test and solution for the human, cat, dog year

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,9 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    var humanYears = number;
+    var catYears = 15 + 9 + ((humanYears-2)*4);
+    var dogYears = 15 + 9 + ((humanYears-2)*5);
+    
+    return [humanYears, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require("../src");
 
 // Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("return an array of three numbers. humanYears, catYears, dogYears", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    })
+})
